71b0cb2254cbfcab021b008a34212d3e94fa552a
marvin obig <obig.marv@gmail.com>
2022-01-22 11:41:17 +0100

    added mobile nav hamburger functionality and made small changes to font sizes

--- script/graphic-design-portfolio.js
+++ script/graphic-design-portfolio.js
@@ -1,1 +1,13 @@
+let nav = document.getElementById("hamburger");
+nav.addEventListener("click", navigation);
 
+
+function navigation() {
+    let navLinks = document.getElementById("navLinks");
+
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+    }else {
+        navLinks.style.display = "none";
+    }
+}
